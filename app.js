d0e840978d2960c07dbe0f44f17dfca3f40a7ec8
lcsharp <lcsharp@us.ibm.com>
2017-11-26 11:15:54 -0500

    add database and use request to get data from the db

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const port = 8080;
 // Init app
 const app = express();
 
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/todoapp';
+
 // Body Parser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -17,10 +20,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res, next) => {
-	res.render('index');
+//Connect to MongoDB
+MongoClient.connect(url, (err, database) => {
+	console.log('MongoDB Connected...');
+	if (err) {
+		throw err;
+	}
+	db = database;
+	Todos = db.collection('todos');
+
+	app.listen(port, () => {
+		console.log('Server running on port ', port);
+	});
 });
 
-app.listen(port, () => {
-	console.log('Server running on port ', port);
+app.get('/', (req, res, next) => {
+	Todos.find({}).toArray((err, todos) => {
+		if (err) {
+			return console.log(err);
+		}
+		console.log(todos);
+		res.render('index', {
+			todos: todos
+		});
+		// res.render('index', {
+		// 	todos: todos
+		// });
+	});
 });
