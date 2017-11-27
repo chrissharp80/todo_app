131b28783f7205ddc50ae5d7d9e587b43ac4a024
lcsharp <lcsharp@us.ibm.com>
2017-11-26 20:11:59 -0500

    add edit capability

--- app.js
+++ app.js
@@ -75,3 +75,33 @@ app.delete('/todo/delete/:id', (req, res, next) => {
 		res.send(200);
 	});
 });
+
+app.get('/todo/edit/:id', (req, res, next) => {
+	const query = {_id: ObjectID(req.params.id)};
+	Todos.find(query).next((err, todo) => {
+		if (err) {
+			return console.log(err);
+		}
+		res.render('edit', {
+			todo: todo
+		});
+	});
+});
+
+app.post('/todo/edit/:id', (req, res, next) => {
+	const query = {_id: ObjectID(req.params.id)};
+	// Create todo
+	const todo = {
+		text: req.body.text,
+		body: req.body.body
+	};
+
+	// Update todo
+	Todos.updateOne(query, {$set:todo}, (err, result) => {
+		if (err) {
+			return console.log(err);
+		}
+		console.log('Todo Updated...');
+		res.redirect('/');
+	});
+});
